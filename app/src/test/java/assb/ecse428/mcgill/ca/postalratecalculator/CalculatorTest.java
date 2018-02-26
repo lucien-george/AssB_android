49daf4be2de234e8c971e100e4e211ff192234ca
mikym2 <maatouk.michael@gmail.com>
2018-02-26 17:49:41 -0500

    Approving test for letterMail type

--- app/src/test/java/assb/ecse428/mcgill/ca/postalratecalculator/CalculatorTest.java
+++ app/src/test/java/assb/ecse428/mcgill/ca/postalratecalculator/CalculatorTest.java
@@ -1,8 +1,20 @@
 package assb.ecse428.mcgill.ca.postalratecalculator;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by User on 2/26/2018.
  */
 
 public class CalculatorTest {
+    @Test
+    public void getLetterMailTypeTest() throws Exception{
+
+        Calculator cal = new Calculator() ;
+        assertEquals(cal.getLetterMailType("200","100","3","10"), Calculator.LetterMail.LETTER);
+
+
+    }
 }
