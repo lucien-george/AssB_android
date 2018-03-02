af047875ffdbb4718ebb2e996e97a3df309a11aa
mikym2 <maatouk.michael@gmail.com>
2018-03-02 17:28:10 -0500

    getPostalRateLetterMailTest - Invalid - done

---
+++ app/src/test/java/assb/ecse428/mcgill/ca/postalratecalculator/MainActivityTest.java
@@ -0,0 +1,29 @@
+package assb.ecse428.mcgill.ca.postalratecalculator;
+
+import org.junit.Test;
+
+/**
+ * Created by User on 3/2/2018.
+ */
+
+public class MainActivityTest {
+
+    @Test
+    public void check_inputsTest() throws Exception{
+   /*     boolean answer = true;
+        Bundle bundle = new Bundle();
+        MainActivity mainActivity = new MainActivity();
+        mainActivity.onCreate(bundle);
+        mainActivity.setUpVariables();
+
+        mainActivity.from_et.setText("H2P3K8");
+        mainActivity.to_et.setText("H2P3K9");
+        mainActivity.length_et.setText("10");
+        mainActivity.width_et.setText("10");
+        mainActivity.height_et.setText("10");
+        mainActivity.weight_et.setText("100");*/
+      //  assertEquals(mainActivity.check_inputs(),answer);
+
+
+    }
+}
